317608d56981d3189038551e66b8e1087a4ed2ab
Jose Mercado <josuemercado32@gmail.com>
2022-12-25 02:20:51 -0500

    .

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import flet as ft
 import moviepy as mv
+import pytube
 from pytube import YouTube
 
 
@@ -11,36 +12,42 @@ def main(page:ft.Page):
     page.window_width = 320
     page.window_height = 570
     page.window_resizable = False
-    page.theme_mode='light'
+    page.theme_mode='dark'
     page.update()
 
     page.appbar = ft.AppBar(
         leading=ft.Icon(color='BLACK'),
         leading_width=5,
-        title=ft.Text("Downloader :v"),
+        title=ft.Text("Downloader"),
         center_title=True,
-        toolbar_height=35, #tamaño
-        color='black', #color de letra
-        bgcolor='white', #color de fondo
+        toolbar_height=30, #tamaño
+        color='white', #color de letra
+        bgcolor='pink', #color de fondo
         actions=[
-            ft.IconButton(icon='SUNNY')
+            ft.IconButton(icon='SUNNY', icon_size=17)
         ] )
     def textchange(e):
-        try:
-            video_details=YouTube(url=e.control.value)
-            print(video_details)
-            print(video_details.title)
-        except:
-            print(txt)
-
+        link = e.control.value
+        yt = YouTube(link)
+        print(yt)
+    def descarga():
+        pass
     txt = ft.TextField(
-        label='link', on_change=textchange, cursor_color='red', prefix_icon='LINK', keyboard_type='url', color='black', border='underline'
+        label='link',
+        on_submit=textchange,
+        cursor_color='red',
+        #prefix_icon='LINK',
+        keyboard_type='url',
+        color='white',
+        border='underline',
+        filled=True,
     )
     image = ft.Image(
+        #src='https://cdn-icons-png.flaticon.com/512/1383/1383327.png',
         src='https://cdn-icons-png.flaticon.com/512/725/725300.png',
         width=140,
     )
-    boton = ft.ElevatedButton(text='Descargar video')
+    boton = ft.ElevatedButton(text='Descargar video', on_click=descarga, icon='DOWNLOAD_FOR_OFFLINE')
 
     page.add(
         image,
